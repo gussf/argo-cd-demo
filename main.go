4f70ef9c8445e6cdac82449aac7a9ee9f6b4067e
Gustavo <25396922+gussf@users.noreply.github.com>
2021-10-04 23:04:13 -0300

    New app version!

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 func main() {
 
 	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
-		rw.Write([]byte("Welcome to my demo :)"))
+		rw.Write([]byte("Welcome to my demo :) v2"))
 	})
 	http.ListenAndServe("0.0.0.0:9000", nil)
 }
